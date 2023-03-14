4b9cc826e3e69953393e3fbf7cccd1fa58f3e1f6
Mickael.C <mickael.correia@lagrandeclasse.fr>
2023-03-14 16:29:38 +0100

    minor modif

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.post('/login', async (req, res) => {
                 id: userDoc._id
             }, jwtSecret, {}, (err, token) => {
                 if (err) throw err;
-                res.cookie('token', token, { httpOnly: true, secure: true }).json(userDoc);
+                res.cookie('token', token, { httpOnly: true, sameSite: 'none', secure: true }).json(userDoc);
 
             });
         } else {
